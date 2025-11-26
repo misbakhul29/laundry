cd9a828f122b0006dc921922aef0434d3d430f38
misbakhul29 <misbakhul2904@gmail.com>
2025-11-26 19:34:38 +0700

    fix: build error cause prisma wrong prisma import

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -3,13 +3,10 @@ import Credentials from "next-auth/providers/credentials";
 import Google from "next-auth/providers/google";
 import { PrismaAdapter } from "@auth/prisma-adapter";
 import bcrypt from "bcrypt";
-import { PrismaClient } from "@prisma/client/extension";
-
-const prismaClient = new PrismaClient();
-const db = prismaClient.prototype;
+import prisma from "@/lib/prisma";
 
 const handler = NextAuth({
-  adapter: PrismaAdapter(prismaClient.prototype),
+  adapter: PrismaAdapter(prisma),
   session: {
     strategy: "jwt",
   },
@@ -30,7 +27,7 @@ const handler = NextAuth({
       async authorize(credentials) {
         if (!credentials?.email || !credentials.password) return null;
 
-        const user = await db.user.findUnique({
+        const user = await prisma.user.findUnique({
           where: { email: credentials.email },
         });
         if (!user) return null;
@@ -41,7 +38,7 @@ const handler = NextAuth({
         return {
           id: user.id,
           email: user.email,
-          name: user.name,
+          name: user.username,
         };
       },
     }),
