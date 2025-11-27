44ee97b885c7aca2c2dab212d847a62b06019cab
misbakhul29 <misbakhul2904@gmail.com>
2025-11-27 19:43:10 +0700

    fix: deployment action

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,12 +1,10 @@
 import { PrismaClient } from "./generated/prisma/client";
 
 declare global {
-  // allow global `var` in dev to persist PrismaClient across module reloads
-  // eslint-disable-next-line no-var
   var prisma: PrismaClient | undefined;
 }
 
-const prisma = global.prisma || new PrismaClient();
+const prisma = global.prisma
 
 if (process.env.NODE_ENV !== 'production') global.prisma = prisma;
 
